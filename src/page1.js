484ebc56c2dc3551efb0382f3f1baa5ec84b9c3c
Krzysztof Zielinski <krzysztof.zielinski.work@gmail.com>
2017-03-08 00:42:43 +0100

    vip added bootstrap buttons, create simple table and map function

--- src/page1.js
+++ src/page1.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import {Grid, Button} from 'react-bootstrap'
+import {Users} from './page2'
 
-const Page1 = () => (
+const Page1 = (props) => (
 
-  <div>Page 1</div>
-)
+  <Grid>
+    <h1>Page 1</h1>
+
+    <Button>Button</Button>
+    <Button className="btn-danger">Danger</Button>
+    <Button className="btn-success">Success</Button>
+    <Button className="btn-primary">Primary</Button>
+    <Button className="btn-info">Info</Button>
+    <Button className="btn-link">Link</Button>
+    <Button className="btn-warning">Warning</Button>
+    <Button className="btn-default">Default</Button>
+    <Button className="btn-block">Block</Button>
+    <ul>
+      {
+        props.users === undefined ?
+          'Sorry, we are missing props here' :
+          (
+            props.users.length === 0 ?
+              'No users for me' :
+              props.users.map(
+                user => (
+                  <li>
+                    {user.name}
+                  </li>
+                )
+              )
+          )
+      }
+    </ul>
+
+  </Grid>
+);
 
 export default Page1
--- src/page2.js
+++ src/page2.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import {Grid} from 'react-bootstrap'
 
-const Page2 = () => (
+export const Users = [
+  {
+    id: 1,
+    name: 'John',
+    surname: 'Doe',
+    age: 20
+  },
+  {
+    id: 2,
+    name: 'Joline',
+    surname: 'Blabla',
+    age: 22
+  }
+];
 
-  <div>Page 2</div>
-)
+const Page2 = (props) =>
+  (
+
+    <Grid>
+
+      <h1>Users list:</h1>
+      <ul>
+        {
+          Users.map(
+            user => (
+              <li key={user.id}>
+                {user.name}
+              </li>
+            )
+          )
+        }
+      </ul>
+
+    </Grid>
+  );
+
 
 export default Page2
